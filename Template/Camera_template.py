e5efaf3bb4ec41dcdca6530635f4203c4256516e
NicholasYe <2216393769@qq.com>
2021-08-16 21:08:39 +0800

    8:16

--- Template/Camera_template.py
+++ Template/Camera_template.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 # name classifier
-faceCascade = cv2.CascadeClassifier('/home/pi/Opencv/haarcascade_frontalface_default.xml')
+Bottles = cv2.CascadeClassifier('/home/pi/Opencv/haarcascade_frontalface_default.xml')
 
 # name class
 cap = cv2.VideoCapture(0)
@@ -18,23 +18,21 @@ while True:
     ret, img = cap.read()
 
     # image disposal
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # special features
-    faces = faceCascade.detectMultiScale(
-        gray,
+    bottle = Bottles.detectMultiScale(
+        img,
         scaleFactor=1.2,
         minNeighbors=5,
         minSize=(20, 20)
     )
 
     # identify the object with frame
-    for (x, y, w, h) in faces:
+    for (x, y, w, h) in bottle:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
 
-    # display the image window with title
+    # display the camera window with title
     cv2.imshow('I am so tired!!', img)
 
     # close the window
