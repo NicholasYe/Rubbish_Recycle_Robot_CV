209d37921fc7baf3e0f801936f4f9e09e95bb26d
NicholasYe <2216393769@qq.com>
2021-09-07 03:04:56 +0800

    9.7

--- Car_version/Transmit.py
+++ Car_version/Transmit.py
@@ -1,8 +1,20 @@
 import numpy as np
 import cv2
 
-def detectHSColor():
-    print("a")
+def detectHSColor(in_image, minHue, maxHue, minSat, maxSat):
+    hsv = cv2.cvtColor(in_image, code = cv2.COLOR_BGR2HSV)
+    
+    channels = np.array(Zero_array)
+    cv2.split(hsv, channels)
+    mask1 = cv2.threshold(channels[0], maxHue, 255, type = cv2.THRESH_BINARY_INV)
+    mask2 = cv2.threshold(channels[0], minHue, 255 ,typr = cv2.THRESH_BINARY)
+    if minHue < maxHue :
+        hueMask = mask1 & mask2
+    else :
+        hueMask = mask1 | mask2
+    satMask = cv2.inRange(channels[1], minSat, maxSat)
+    mask = hueMask & satMask
+    return mask
 
 # name path
 Class_path = '/home/pi/Rubbish_Recycle_Robot_CV/Xml/8.29_Generation_Three/cascade8.29.xml'
@@ -24,11 +36,14 @@ while True:
     ret, img = cap.read()
 
     # image disposal
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    image_green = detectHSColor(img, 35, 77, 43, 255)
+    image_process = img.copyTo(image_green)
+    image_gray = cv2.cvtColor(image_process, cv2.COLOR_BAYER_BG2GRAY)
 
     # special features
     bottle = Bottle.detectMultiScale(
-        gray,
+        image_gray,
         scaleFactor=1.2,
         minNeighbors=20,
         minSize=(20, 20),
